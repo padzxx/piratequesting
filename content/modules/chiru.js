d0eb180813b5119993b8aef5f77d2dea49653857
finglandj <finglandj@gmail.com>
2011-06-01 19:24:59 +0000

    Made significantly more efficient. uses a single timer for the page rather than one for each row.

--- content/modules/chiru.js
+++ content/modules/chiru.js
@@ -19,55 +19,54 @@ piratequesting.Chirugeon = {
 		// regex used when converting FM's time remaining into seconds
 		var timex = /([0-9]+)m\s*([0-9]+)s/i;
 		var idex = /user=([0-9]+)/i;
-		var ca = doc.getElementById("contentarea");
-		var chirutable = ca.getElementsByTagName("table")[0];
-		var rows = chirutable.getElementsByTagName("tr");
-		// note row[0] is the heading information.
-		var cells, cell, time, seconds, timers = [], obj, player_id;
-		cell = doc.createElement("th");
-		cell.appendChild(doc.createTextNode("Actions"));
-		rows[0].appendChild(cell);
-		for (var i = 1; i < rows.length; i++) {
-			try {
-				// now get the last cell
-				cells = rows[i].getElementsByTagName("td");
-				player_id = idex.exec(cells[1].innerHTML)[1];
-				cell = cells[cells.length - 1];
-				time = timex.exec(cell.firstChild.nodeValue);
-				seconds = (Number(time[1]) * 60) + Number(time[2]);
-				obj = new Timer();
-				obj.index = Number(seconds);
-				obj.Interval = 1000;
-				obj.document = doc;
-				obj.target = cell;
-				obj.Tick = timer_tick;
-				obj.Start();
-				timers.push(obj);
-				cell = doc.createElement("td");
-				cell.innerHTML = "<a href='/index.php?on=attack&id="
-						+ player_id
-						+ "'>" + piratequesting.strings.Chirugeon
-						.getString("attack") +"</a> <a href='/index.php?on=mug&id="
-						+ player_id + "'>" + piratequesting.strings.Chirugeon
-						.getString("mug") + "</a>";
-				rows[i].appendChild(cell);
-			} catch (e) {
-				// fubar. let's die quietly
+		var time_cells = [];
+		try {
+			var ca = doc.getElementById("contentarea");
+			var chirutable = ca.getElementsByTagName("table")[0];
+			var rows = chirutable.getElementsByTagName("tr");
+			// note row[0] is the heading information.
+			var cell, cells,action_cell, time, time_cell, obj, player_id;
+			cell = doc.createElement("th");
+			cell.appendChild(doc.createTextNode("Actions"));
+			rows[0].appendChild(cell);
+			for ( var i = 1; i < rows.length; i++) {
+				try {
+					// now get the last cell
+					cells = rows[i].getElementsByTagName("td");
+					player_id = idex.exec(cells[1].innerHTML)[1];
+					time_cell = cells[cells.length - 1];
+					time = timex.exec(time_cell.innerHTML);
+					time_cell.pqseconds = (Number(time[1]) * 60) + Number(time[2]);  
+					time_cells.push(time_cell);
+					action_cell = doc.createElement("td");
+					action_cell.innerHTML = "<a href='/index.php?on=attack&id=" + player_id + "'>" + piratequesting.strings.Chirugeon.getString("attack") + "</a> <a href='/index.php?on=mug&id=" + player_id + "'>" + piratequesting.strings.Chirugeon.getString("mug") + "</a>";
+					rows[i].appendChild(action_cell);
+				} catch (e) {
+					dumpError(e);
+				}
 			}
+			obj = new Timer(timer_tick.curry(time_cells));
+			obj.Start();
+			
+		} catch (e) {
+			dumpError(e);
 		}
 
-		function timer_tick() {
-			// alert(this);
-			this.index--;
-			this.target.innerHTML = Math.floor(this.index / 60) + "m "
-					+ (this.index % 60) + "s";
-
-			if (this.index == 0) {
-				this.Stop();
-				delete this;
+		function timer_tick(cells, seconds) {
+			var time_cell, time, seconds, time_left;
+			var all_done = true;
+			for(var i=0, len=cells.length; i<len;++i) {
+				time_cell = cells[i];
+				time_left = --time_cell.pqseconds;
+				if (time_left >= 0) {
+					time_cell.innerHTML = Math.floor(time_left / 60) + "m " + (time_left % 60) + "s";
+					all_done = false;
+				}
+			}
+			if (all_done) {
+				obj.Stop();
 			}
 		}
-
 	}
 };
 
