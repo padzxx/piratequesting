34c03f8bda8e342b84eb47ca9969a85525515002
finglandj <finglandj@gmail.com>
2011-06-01 15:19:05 +0000

    Changes forum signature size preference check to use the preselected branch in piratequesting.prefs rather than the whole pref name

--- content/modules/forum.js
+++ content/modules/forum.js
@@ -3,10 +3,9 @@ var piratequesting = top.piratequesting;
 piratequesting.Forum = {
 	process : function(doc) {
 		var oNewStyle = doc.createElement("style");
-		oNewStyle.innerHTML = 'td.post2 hr + small { display:block; max-height:' + piratequesting.prefs.getIntPref("extensions.piratequesting.ForumSigsMaxSize") + 'px; overflow:auto; }';
+		oNewStyle.innerHTML = 'td.post2 hr + small { display:block; max-height:' + piratequesting.prefs.getIntPref("ForumSigsMaxSize") + 'px; overflow:auto; }';
 		doc.body.appendChild(oNewStyle);
 	}
 };
 
-piratequesting.addLoadProcess(new RegExp("index\\.php\\?on=forum&tid=", ""),
-		piratequesting.Forum.process);
+piratequesting.addLoadProcess(new RegExp("index\\.php\\?on=forum&tid=", ""),piratequesting.Forum.process);
